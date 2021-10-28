12c910c20bdd960f138d125edac97fb32822b4dc
Harsh Goyal <harsh.student99@gmail.com>
2021-10-29 01:25:37 +0530

    Links text added to navbar

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -7,6 +7,12 @@ function Navbar() {
         <div className={styles.navbarLogo}>
             <img style={{height: "100%"}}src="/Images/logo.png" alt="GDSC USICT Logo" />
             </div>
+        <div className={styles.navbarLinks}>
+            <div className={styles.navbarLink}>Home</div>
+            <div className={styles.navbarLink}>About</div>
+            <div className={styles.navbarLink}>Events</div>
+            <div className={styles.navbarLink}>Contact Us</div>
+            </div>        
         </div>
     )
 }
